a1ec7e93a766d8e483c588e8a6a513c80104a356
Caleb Hayashida <santeyio@gmail.com>
2018-03-27 11:18:39 -0400

    stuff

--- app.py
+++ app.py
@@ -7,11 +7,16 @@ from flask import Flask, render_template, request, make_response, abort, jsonify
 app = Flask(__name__)
 
 
-@route('/volume/<adjustment>', methods=["POST"])
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/volume/<adjustment>', methods=["POST"])
 def adjust_volume(adjustment=None):
     """ Adjust the volume on the local machine """
 
-    if adjustment = "up":
+    if adjustment == "up":
         subprocess.call('./scripts/volume_up.sh')
-    elif adjustment = "down":
+    elif adjustment == "down":
         subprocess.call('./scripts/volume_down.sh')
